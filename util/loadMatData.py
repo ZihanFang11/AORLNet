fa0849e1614204940f34a1a1871308614aa9920e
ZihanFang11 <70749751+ZihanFang11@users.noreply.github.com>
2024-11-16 15:42:37 +0800

    Add files via upload

--- util/loadMatData.py
+++ util/loadMatData.py
@@ -3,16 +3,18 @@ from util.hypergraph_utils import construct_H_with_KNN, generate_G_from_H
 import torch
 import numpy as np
 import scipy.io as sio
+import scipy.sparse as sp
 import scipy.sparse as ss
 from sklearn.preprocessing import normalize
-
+from sklearn.neighbors import kneighbors_graph
 def load_data(args, dataset):
     data = sio.loadmat(args.data_path + dataset + '.mat')
     feature = data['X']
     labels = data['Y'].flatten()
     labels = labels - min(set(labels))
     feature = normalize(feature)
-    return feature, labels
+    adj = data['adj']
+    return feature, labels, adj
 
 def sparse_mx_to_torch_sparse_tensor(sparse_mx):
     """Convert a scipy sparse matrix to a torch sparse tensor."""
@@ -47,3 +49,32 @@ def construct_hypergraph(dataset,feature, knn,device):
         lap=torch.from_numpy(temp_lap[0]).to(torch.float32).to(device)
         adj=torch.from_numpy(temp_adj[0]).to(torch.float32).to(device)
     return lap,adj
+def construct_hypergraph_noise(dataset,feature, knn,device):
+
+    print("Constructing the hyperlap matrix of "  + dataset)
+    H = construct_H_with_KNN([feature], knn, split_diff_scale=True)
+    G,temp_adj = generate_G_from_H(H)
+    temp_lap = np.identity(len(G)) - G
+    lap=torch.from_numpy(temp_lap[0]).to(torch.float32).to(device)
+    adj=torch.from_numpy(temp_adj[0]).to(torch.float32).to(device)
+    return lap,adj
+def construct_laplacian(adj):
+    """
+        construct the Laplacian matrix
+    :param adj: original Laplacian matrix  <class 'scipy.sparse.csr.csr_matrix'>
+    :return:
+    """
+    adj_ = sp.coo_matrix(adj)
+    rowsum = np.array(adj_.sum(1))
+    degree_mat_inv_sqrt = sp.diags(np.power(rowsum, -0.5).flatten())  # degree matrix
+    adj_wave = adj_.dot(degree_mat_inv_sqrt).transpose().dot(degree_mat_inv_sqrt)
+    lp = sp.eye(adj.shape[0]) - adj_wave
+    return lp
+
+def features_to_Lap(features, knns=10):
+    temp = kneighbors_graph(features, knns)
+    temp = sp.coo_matrix(temp)
+    # build symmetric adjacency matrix
+    temp = temp + temp.T.multiply(temp.T > temp) - temp.multiply(temp.T > temp)
+    lap=sparse_mx_to_torch_sparse_tensor(construct_laplacian(temp))
+    return lap
